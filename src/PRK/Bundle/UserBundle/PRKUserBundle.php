7b5545bd9093e368793a2d6e4acece52cea6d589
james <james@james-PC>
2014-12-28 21:19:08 +0100

    prk user bundle

---
+++ src/PRK/Bundle/UserBundle/PRKUserBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PRK\Bundle\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PRKUserBundle extends Bundle
+{
+     public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
+}
